2abb3ebebaacbba49798a6cc1ab0ea86909d4825
zonzz <88747756+zonzzlive@users.noreply.github.com>
2022-09-27 15:50:30 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ const port = 3000
 
 app.get('/', (req,res) => res.send('Hello, Express'))
 
+app.get('/api/pokemons/:id', (req,res) =>{
+    const id = req.params.id
+    res.send(`Pokemon n°${id}`)
+})
+
 app.listen(port, () => console.log(`Notre application Node est démarée sur : https://localhost:${port}`))
